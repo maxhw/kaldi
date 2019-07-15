680c3193108d1a0d0de0c681d4fdb1371cc92a2a
Xiao Haiwen <haiwen.xiao@gmail.com>
2019-07-16 02:58:12 +0800

    add more training

--- egs/cmdWord/s5/run.sh
+++ egs/cmdWord/s5/run.sh
@@ -31,17 +31,53 @@ done
 # Mono training
 #steps/train_mono.sh --nj $n --cmd "$train_cmd" --totgauss 400 data/train data/lang exp/mono0a
 steps/train_mono.sh --nj $n --cmd "$train_cmd" --boost-silence 3.25 data/train data/lang exp/mono0a
-
 #test monophone model
 local/decode.sh --mono true --nj $n "steps/decode.sh" exp/mono0a data
 
+
 #monophone_ali
-steps/align_si.sh --nj $n --cmd "$train_cmd" --boost-silence 3.25 data/train data/lang exp/mono0a exp/mono_ali || exit 1;
+steps/align_si.sh --nj $n --cmd "$train_cmd" --boost-silence 3.25 data/train data/lang exp/mono0a exp/mono0a_ali || exit 1;
 
 #triphone
-steps/train_deltas.sh --cmd "$train_cmd" --boost-silence 3.25 2000 10000 data/train data/lang exp/mono0a exp/tri1 || exit 1;
-
+steps/train_deltas.sh --cmd "$train_cmd" --boost-silence 3.25 2000 10000 data/train data/lang exp/mono0a_ali exp/tri1 || exit 1;
 #test tri1 model
 local/decode.sh --nj $n "steps/decode.sh" exp/tri1 data
+
+
+#triphone_ali
+steps/align_si.sh --nj $n --cmd "$train_cmd" data/train data/lang exp/tri1 exp/tri1_ali || exit 1;
+
+#lda_mllt
+steps/train_lda_mllt.sh --cmd "$train_cmd"  --splice-opts "--left-context=3 --right-context=3" 2500 15000 data/train data/lang exp/tri1_ali exp/tri2b || exit 1;
+#test tri2b model
+local/decode.sh --nj $n "steps/decode.sh" exp/tri2b data
+
+
+#lda_mllt_ali
+steps/align_si.sh  --nj $n --cmd "$train_cmd" --use-graphs true data/train data/lang exp/tri2b exp/tri2b_ali || exit 1;
+
+#sat
+steps/train_sat.sh --cmd "$train_cmd" 2500 15000 data/train data/lang exp/tri2b_ali exp/tri3b || exit 1;
+#test tri3b model
+local/decode.sh --nj $n "steps/decode_fmllr.sh" exp/tri3b data
+
+
+#sat_ali
+steps/align_fmllr.sh --nj $n --cmd "$train_cmd" data/train data/lang exp/tri3b exp/tri3b_ali || exit 1;
+
+#quick
+steps/train_quick.sh --cmd "$train_cmd" 4200 40000 data/train data/lang exp/tri3b_ali exp/tri4b || exit 1;
+#test tri4b model
+local/decode.sh --nj $n "steps/decode_fmllr.sh" exp/tri4b data
+
+
+#quick_ali
+steps/align_fmllr.sh --nj $n --cmd "$train_cmd" data/train data/lang exp/tri4b exp/tri4b_ali || exit 1;
+
+#quick_ali_cv
+#steps/align_fmllr.sh --nj $n --cmd "$train_cmd" data/dev data/lang exp/tri4b exp/tri4b_ali_cv || exit 1;
+
+#train dnn model
+#local/nnet/run_dnn.sh --stage 0 --nj $n  exp/tri4b exp/tri4b_ali exp/tri4b_ali_cv || exit 1;
 
 for x in exp/*/decode*; do [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh; done
